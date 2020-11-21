fa610b54d7764acc9d9285977d26fef6bc2fac28
xternet <majewski.post@gmail.com>
2020-11-21 14:21:00 +0100

    .

---
+++ README.md
@@ -0,0 +1,4 @@
+## Supply chain DApp
+
+## 🔧 Project Diagram:
+![Project Diagram/Workflow](https://i.gyazo.com/c65dd56bc1b4dc79f2ebfd10599d15a6.png)
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react'
 import Identicon from 'identicon.js';
+import logo from '../logo.png'
 
 class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow text-monospace">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           href="http://www.dappuniversity.com/bootcamp"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Dapp University Supply Chain
+          <img src={logo} width="30" height="30" className="align-top" alt="" />
+          &nbsp;$upply_Ch@in
         </a>
-
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
               <small id="account">{this.props.account}</small>
             </small>
-
             { this.props.account
               ? <img
                 className="ml-2"
@@ -31,7 +31,6 @@ class Navbar extends Component {
               />
               : <span></span>
             }
-
           </li>
         </ul>
       </nav>
